c62a1edc5dc631b52917eb3a97961e06d5db5155
omkarhale <omkarhale258210@gmail.com>
2024-08-18 12:17:04 +0530

    firstly ,setup the db & created packages

--- src/main/java/com/stream/app/entites/Video.java
+++ src/main/java/com/stream/app/entites/Video.java
@@ -18,6 +18,46 @@ public class Video {
     private String description;
     private String contentType;
     private String filePath;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setVideoId(String videoId) {
+        this.videoId = videoId;
+    }
+
+    public String getVideoId() {
+        return videoId;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
 //    @ManyToOne
 //    private Course course;
 }
--- src/main/java/com/stream/app/services/Impl/VideoServicesImpl.java
+++ src/main/java/com/stream/app/services/Impl/VideoServicesImpl.java
@@ -1,17 +1,90 @@
 package com.stream.app.services.Impl;
 
 import com.stream.app.entites.Video;
+import com.stream.app.repositiories.VideoRepository;
 import com.stream.app.services.VideoService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 @Service
 public class VideoServicesImpl implements VideoService {
+
+
+    @Value("${files.video}")
+    String DIR;
+
+
+    private VideoRepository videoRepository;
+
+    public VideoServicesImpl(VideoRepository videoRepository) {
+        this.videoRepository = videoRepository;
+    }
+
+    @PostConstruct
+    public void init(){
+
+        File file=new File(DIR);
+        if (!file.exists()){
+            file.mkdir();
+            System.out.println("Folder Created:");
+        }else{
+            System.out.println("Folder already created");
+        }
+
+    }
     @Override
     public Video save(Video video, MultipartFile file) {
-        return null;
+          //original file name
+
+        try {
+            String filename = file.getOriginalFilename();
+            String contentType = file.getContentType();
+            InputStream inputStream = file.getInputStream();
+
+
+            //file path
+            String cleanFileName=StringUtils.cleanPath(filename);
+
+             //folder path:create
+            String cleanFolder=StringUtils.cleanPath(DIR);
+
+            //folder path with filename
+            Path path= Paths.get(cleanFolder,cleanFileName);
+
+            System.out.println(contentType);
+            System.out.println(path);
+
+
+            //COPY FILE TO THE FOLDER
+            Files.copy(inputStream,path, StandardCopyOption.REPLACE_EXISTING);
+
+            //VIDEO META DATA
+
+            video.setContentType(contentType);
+            video.setFilePath(path.toString());
+
+            //META DATA SAVE
+
+             return videoRepository.save(video);
+
+        }catch(IOException e){
+            e.printStackTrace();
+            return  null;
+        }
+
     }
 
     @Override
